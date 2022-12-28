dee001afff0e4967793c78e1f5027a49c4ecd30e
Ljupco Nikolov <ljupce@nextsense.com>
2022-12-28 19:49:49 +0100

    Initial Commit

---
+++ iPollingSystem/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iPollingSystem
+//
+//  Created by Nextsense on 12/28/22.
+//  Copyright © 2022 Nextsense. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
